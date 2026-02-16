4b09c805bc846dbf5605c99c9d33852d432b3912
rdanyit <dany.ityarsisumbar@gmail.com>
2026-02-16 12:11:59 +0700

    Update Portal PMB dan menambahkan Form Daftar

--- asset/js/hide-psw.js
+++ asset/js/hide-psw.js
@@ -1,9 +1,24 @@
-const togglePassword = document.getElementById("togglePassword");
-const passwordInput = document.getElementById("password");
+// const togglePassword = document.getElementById("togglePassword");
+// const passwordInput = document.getElementById("password");
 
-togglePassword.addEventListener("click", () => {
-  const isPassword = passwordInput.type === "password";
+// togglePassword.addEventListener("click", () => {
+//   const isPassword = passwordInput.type === "password";
 
-  passwordInput.type = isPassword ? "text" : "password";
-  togglePassword.textContent = isPassword ? "👁" : "🙈";
+//   passwordInput.type = isPassword ? "text" : "password";
+//   togglePassword.textContent = isPassword ? "👁" : "🙈";
+// });
+
+document.addEventListener("click", function (e) {
+  const toggle = e.target.closest(".toggle-password");
+  if (!toggle) return;
+
+  const wrapper = toggle.closest(".input-password");
+  const input = wrapper.querySelector(".password-input");
+
+  if (!input) return;
+
+  const isHidden = input.type === "password";
+  input.type = isHidden ? "text" : "password";
+
+  toggle.textContent = isHidden ? "👁" : "🙈";
 });
